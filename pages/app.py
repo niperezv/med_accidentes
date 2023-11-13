2a1c74be783b00b4c3e894d05c1f3e67cae763ef
pePOOni <47955066+niperezv@users.noreply.github.com>
2023-11-13 20:52:53 +0000

    final

--- pages/app.py
+++ pages/app.py
@@ -204,8 +204,8 @@ with tab3:
     'cluster_4' : 'Baja',
     'cluster_5': 'Muy baja'
     }
-    ruta_html1 = 'mapa_grupoJ.html'
-    ruta_html = 'pages/mapa_grupoJ.html'
+    ruta_html1 = 'med_accidentes/mapa_grupoJ.html'
+    ruta_html = 'mapa_grupoJ.html'
     with open(ruta_html1, "r", encoding="utf-8") as file:
         contenido_html = file.read()
         #st.write("Ruta del archivo HTML:", ruta_html)
