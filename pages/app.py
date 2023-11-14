ae155d34d6cbb00eaf2c76d72909c97defea1260
Stiv567 <146900193+Stiv567@users.noreply.github.com>
2023-11-13 19:45:44 -0500

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -80,13 +80,18 @@ def load_df2(year,type_a):
     st.write(data_fecha_especifica) 
     return data_fecha_especifica
 
+def load_df3(year):
+    data_fecha_especifica = datam[datam['ANO']== year]
+    st.write(data_fecha_especifica)
+    return data_fecha_especifica
+
 tab1, tab2, tab3 = st.tabs(["Datos historicos", "Predecir Accidentalidad", "Mapa accidentalidad"])
 
 with tab1:
     st.header("Datos historicos")
     options = st.selectbox(
    "Que tipo de accidente desea visualizar?",
-   ("Atropello", "Caída de Ocupante", "Choque", "Incendio", "Volcamiento", "Otro"),
+   ("Atropello", "Caída de Ocupante", "Choque", "Incendio", "Volcamiento", "Otro","No diferenciar por tipo"),
    index=0,
    placeholder="Seleccionar tipo accidente...",
    )
@@ -103,8 +108,30 @@ with tab1:
     if option_ano is not None:  # Verifica que se haya seleccionado un año
         option_ano = int(option_ano)
         data_actual = load_df2(option_ano, options)
+        data_actual2 = load_df3(option_ano)
 
-        if data_actual is not None:
+        if data_actual is not None and options == "No diferenciar por tipo":
+            if checkbox_diario:
+                st.subheader("Diariamente")
+                date_range = pd.date_range(start=data_actual2.loc[data_actual2.index[0], "FECHA"], end=data_actual2.loc[data_actual2.index[-1], "FECHA"])
+                day_counts = data_actual2['FECHA'].dt.date.value_counts().reindex(date_range, fill_value=0)
+                st.bar_chart(day_counts)
+            else:
+                st.info("Marca la casilla 'Diariamente' para ver la gráfica diaria.")
+            if checkbox_semanal:
+                st.subheader("Semanalmente")
+                weekly_counts = data_actual2['SEMANA'].value_counts().sort_index()
+                st.bar_chart(weekly_counts)
+            else:
+                st.info("Marca la casilla 'Semanalmente' para ver la gráfica semanal.")
+            if checkbox_mensual:
+                st.subheader("Mensualmente")
+                month_counts = data_actual2['MES'].value_counts().sort_index()
+                st.bar_chart(month_counts)
+            else:
+                st.info("Marca la casilla 'Mensualmente' para ver la gráfica anual.")
+                
+        elif data_actual is not None:
             #metricas
             #diario
             if checkbox_diario:
